5fb8c0f6ac8b4df81213f01df1a26632b31e6636
FlowerDeLuce <zly4e4ka666@rambler.ru>
2017-05-16 10:19:33 +0300

    landing home
    first section

---
+++ app/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	$('.banner__slider').slick({
+		nextArrow: '.banner__slider__next',
+		prevArrow: '.banner__slider__prev',
+dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  
+	});
+	
+	
+});
---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass');
+	browserSync = require('browser-sync');
+
+	gulp.task('sass', function(){
+		return gulp.src('app/sass/main.sass')
+		.pipe(sass())
+		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.reload({stream: true}))
+	});
+	
+	
+	gulp.task('browser-sync', function(){
+		browserSync({
+			server: {
+				baseDir: 'app'
+			}
+		});
+		
+	});
+	
+	gulp.task('watch',['browser-sync', 'sass'], function(){
+		gulp.watch('app/sass/*.sass', ['sass']);
+		gulp.watch('app/*.html', browserSync.reload);
+	});
